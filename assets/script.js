33e980aeb8b6aba733314502e8892badbf4101ba
Bryce Speirs <baspeirs@gmail.com>
2020-04-27 20:51:52 -0400

    styled the page a bit with css and bootstrap components, organized html and completed first step of the scrip with api calls

---
+++ assets/script.js
@@ -0,0 +1,119 @@
+// id's 
+//     submitCity
+//     textBox
+//     storeSearch
+//     todaysForecast
+
+//global variables for the api call
+var baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var key = '&appid=00134655df609db935a541653e50ae37';
+var selectCity = '';
+
+$("#submitCity").on("click", searchCity)
+$(".cityButton").on("click", cityButtonSearch)
+
+// submit button id inputGroup-sizing-default
+function searchCity() {
+    let input = $("#textBox").val();
+    selectCity = input;
+    var fullURL = baseURL + selectCity + key;
+    
+
+
+    $.ajax({
+        url: fullURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        $("#todaysForecast").empty()
+
+        var cityButton = $("<button>");
+        cityButton.attr("class", "cityButton")
+        var storeDiv = $("<div>");
+    
+        // store the city name on the left of the page
+        cityButton.text(response.name)
+        storeDiv.append(cityButton)
+        $("#storeSearch").prepend(storeDiv)
+
+        // store todays forecast on the right
+        var cityTag = $("<h1>");
+        var dateTag = $("<h4>")
+        var tempTag = $("<p>");
+        var humidTag = $("<p>");
+        var windTag = $("<p>");
+        var uvIndexTag = $("<p>");
+
+        // append the city
+        var city = response.name;
+        cityTag.text(city);
+        //append the date
+        var date = moment().format("dddd, MMMM YYYY");
+        dateTag.text(date);
+        // convert kelven to degreese and append it
+        var tempKelven = response.main.temp;
+        var convertKelven = (1.8 * (tempKelven - 273) + 32)
+        var tempF = Math.floor(convertKelven)
+        tempTag.text("Temperature: " + tempF + "F")
+        //convert humidity to english and append it
+        var humidity = response.main.humidity + "%";
+        humidTag.text( "Humidity: " + humidity);
+        // convert the wind speed to english and append it
+        var windSpeed = (response.wind.speed + "MPH");
+        windTag.text("Wind speed: " + windSpeed);
+        //append everything to the page
+        $("#todaysForecast").append(cityTag).append(dateTag).append(tempTag).append(humidTag).append(windTag)
+    })
+}
+
+function cityButtonSearch() {
+    selectCity = this.text();
+    var fullURL = baseURL + selectCity + key;
+    
+
+
+    $.ajax({
+        url: fullURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        $("#todaysForecast").empty()
+
+        var cityButton = $("<button>");
+        cityButton.attr("class", "cityButton")
+        var storeDiv = $("<div>");
+    
+        // store the city name on the left of the page
+        cityButton.text(response.name)
+        storeDiv.append(cityButton)
+        $("#storeSearch").prepend(storeDiv)
+
+        // store todays forecast on the right
+        var cityTag = $("<h1>");
+        var dateTag = $("<h4>")
+        var tempTag = $("<p>");
+        var humidTag = $("<p>");
+        var windTag = $("<p>");
+        var uvIndexTag = $("<p>");
+
+        // append the city
+        var city = response.name;
+        cityTag.text(city);
+        //append the date
+        var date = moment().format("dddd, MMMM YYYY");
+        dateTag.text(date);
+        // convert kelven to degreese and append it
+        var tempKelven = response.main.temp;
+        var convertKelven = (1.8 * (tempKelven - 273) + 32)
+        var tempF = Math.floor(convertKelven)
+        tempTag.text("Temperature: " + tempF + "F")
+        //convert humidity to english and append it
+        var humidity = response.main.humidity + "%";
+        humidTag.text( "Humidity: " + humidity);
+        // convert the wind speed to english and append it
+        var windSpeed = (response.wind.speed + "MPH");
+        windTag.text("Wind speed: " + windSpeed);
+        //append everything to the page
+        $("#todaysForecast").append(cityTag).append(dateTag).append(tempTag).append(humidTag).append(windTag)
+    })
+}
